6d735ce8f4a535a2c01b1c1e27e34f48271144a4
Nguyen Anh <me@nguyenanh.net>
2022-05-10 15:21:43 +0700

    Update index

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">ROFI Multiverse!</a>
+          rofi.io
         </h1>
       </main>
 
